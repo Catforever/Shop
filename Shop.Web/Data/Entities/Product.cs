940302fa7d884b96889448052c429afb20513fe3
Isaias Samuel Ramirez Saenz <isaias_samuel.1987@hotmail.com>
2021-02-09 17:25:19 -0600

    Add products controlers

---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace Shop.Web.Data.Entities
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		[Column(TypeName = "decimal(18,2)")]
+		//[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+		public decimal Price { get; set; }
+
+		[Display(Name = "Image")]
+		public string ImageUrl { get; set; }
+
+		[Display(Name = "Last Purchase")]
+		public DateTime LastPurchase { get; set; }
+
+		[Display(Name = "Last Sale")]
+		public DateTime LastSale { get; set; }
+
+		[Display(Name = "Is Availabe?")]
+		public bool IsAvailabe { get; set; }
+
+		[Column(TypeName = "decimal(18,2)")]
+		//[DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+		public double Stock { get; set; }
+	}
+
+}
